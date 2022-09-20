df5a952e8cb997d39681156be30f34d5346f20ae
sharmaashish049@gmail.com <you@example.com>
2022-09-20 23:27:53 +0530

    Day 1 of CPP

---
+++ variablesAndDataTypes.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Data Types in CPP
+
+    // int a = 10;
+    // cout << a;
+    // cout << ": ";
+    // cout << sizeof(a) << endl;
+
+    // char b = 'c';
+    // cout << b;
+    // cout << ": ";
+    // cout << sizeof(b) << endl;
+
+    // bool c = true; // or we can also write 1 & 0 for false both are same
+    // cout << c;
+    // cout << ": ";
+    // cout << sizeof(c) << endl;
+
+    // float d = 1.1;
+    // cout << d;
+    // cout << ": ";
+    // cout << sizeof(d) << endl;
+
+    // double e = 1.234;
+    // cout << e;
+    // cout << ": ";
+    // cout << sizeof(e) << endl;
+
+    // unsigned int a = 5;
+    // cout << a << endl;
+
+    // sizeof to know the memory taken by a datatype
+
+    // Operators in CPP
+
+    int a = 4;
+    int b = 5;
+
+    float c = a / b;
+
+    cout << c << endl;
+    // Logical Operators in CPP Covered
+    // Day1 End
+}
