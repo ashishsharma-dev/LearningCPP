151975db3ced5ab7d1e85084600933a9c620f6d3
sharmaashish049@gmail.com <you@example.com>
2022-09-21 23:15:57 +0530

    Day 2 on conditional and while loop

---
+++ conditionals.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a, b;
+
+    // cout << "Enter the value of A: " << endl;
+    // cin >> a;
+
+    // cout << "Enter the value of B: " << endl;
+    // cin >> b;
+
+    // if (a < 0)
+    // {
+    //     cout << "A is negative" << endl;
+    // }
+    // else if (b < 0)
+    // {
+    //     cout << "B is negative" << endl;
+    // }
+
+    // if (a > b)
+    // {
+    //     cout << "A is greater" << endl;
+    // }
+    // else
+    // {
+    //     cout << "B is greater" << endl;
+    // }
+
+    // char ch;
+    // cout << "Enter a character to check" << endl;
+    // cin >> ch;
+
+    // // Program to identify the character case in CPP
+
+    // if (ch > 65 && ch < 90)
+    // {
+    //     cout << "Uppercase" << endl;
+    // }
+    // else if (ch > 97 && ch < 122)
+    // {
+    //     cout << "Lowercase" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Numeric" << endl;
+    // }
+
+    // Loops in CPP
+
+    // int i = 1;
+    // int n;
+
+    // cout << "Enter the number n: " << endl;
+    // cin >> n;
+
+    // while (i <= n)
+    // {
+    //     cout << "The number is: ";
+    //     cout << i << endl;
+
+    //     i++;
+    // }
+
+    // Sum of all numbers til N in CPP
+
+    // int i = 0;
+    // int n;
+    // int sum = 0;
+    // // Taking input
+    // cout << "Enter the num: ";
+    // cin >> n;
+    // // Calculating Output
+
+    // while (i <= n)
+    // {
+    //     sum = sum + i;
+
+    //     i++;
+    // }
+
+    // cout << "The sum is: ";
+    // cout << sum << endl;
+
+    // Find sum of all even numbers till n
+
+    // int i = 0;
+    // int n;
+    // int sum = 0;
+    // // Taking input
+    // cout << "Enter the num: ";
+    // cin >> n;
+    // // Calculating Output
+
+    // while (i <= n)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         sum = sum + i;
+    //     }
+
+    //     i++;
+    // }
+
+    // cout << "The sum is: ";
+    // cout << sum << endl;
+
+    // Check the number is prime or not
+
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+
+    // int i = 2;
+
+    // if (n == 0 || n == 1)
+    // {
+    //     cout << "Not prime" << endl;
+    // }
+
+    // while (i < n)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << "Not Prime" << endl;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         cout << "Prime" << endl;
+    //         break;
+    //     }
+
+    //     i++;
+    // }
+    // cout << "Prime" << endl;
+}
---
+++ patterns.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int i = 1;
+
+    // Pattern 1                            * * * * *
+    //                                      * * * * *
+    //                                      * * * * *
+    //                                      * * * * *
+    //                                      * * * * *
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    // Pattern 1                            *
+    //                                      * *
+    //                                      * * *
+    //                                      * * * *
+    //                                      * * * * *
+}
